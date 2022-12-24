2c766a9b543406556454ff817934766019617846
laggex <lagchag44@gmail.com>
2022-12-24 13:57:04 +0300

    add new elements

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,28 @@
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=_5aZZSrBTuNGIA2f3XoB6wNw51YzeCvPDNUVxHCczEKeJ_eziUuWujAOvH1embSD_tnEgywpISVlLfqC9w8v9lizRti1Vx7zkZTNkDl4kCnyLa4--WL6l-7JRv9es0JY" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9teS5pbmZvcm1hdGljcy5ydS9hcGkvdjEvbWFudWFscy9tYXRlcmlhbF9maWxlcy8xMzI2MDc0L2Rvd25sb2FkLw"/><template>
+</template>
+<script>
+export default {
+  name: "ChatMessage",
+  props: {
+    username: String,
+  },
+};
+</script>
+<style>
+.chat-message {
+  font-family: "Arial", sans-serif;
+  font-size: 16px;
+  padding: 15px 20px;
+  border-radius: 20px;
+  background: #fff;
+  border: 1px solid #eee;
+  box-shadow: 0 3px 5px 0 #5f5f5f73;
+  color: #16a085;
+  margin-bottom: 15px;
+}
+.chat-message .info {
+  color: #78909c;
+  font-size: 13px;
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/components/MyContainer.vue
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=7C_SkudOQpIpQQDN1BFnnWbIH9sxJSyWFx5eQ2IE_RCMwGF1HzTBc_Y71SNcRgRcAiGCn9JAfW2snPezzw3VTCp1s7jcMnyQHn8bdb8OW_M-GPPRVoAipFMfMmpPiiOg" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9teS5pbmZvcm1hdGljcy5ydS9hcGkvdjEvbWFudWFscy9tYXRlcmlhbF9maWxlcy8xMzI2MDc2L2Rvd25sb2FkLw"/><template>
+<div class="container">
+<slot></slot>
+</div>
+</template>
+<script>
+export default {
+  name: "MyContainer",
+};
+</script>
+<style>
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
